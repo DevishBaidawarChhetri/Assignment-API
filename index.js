75cac9e96b488add8aafd45ded32f26f52cfbbfe
Devish Baidawar Chhetri <devishbaidawar@gmail.com>
2019-12-19 19:36:19 +0545

    Route added in index and Authorization Controller added

--- index.js
+++ index.js
@@ -2,3 +2,13 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const multer = require('multer');
+var swaggerJSDoc=require("swagger-jsdoc");
+var swaggerUI=require("swagger-ui-express");
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.post('/registration',userController.Validator,userController.UserExist, userController.genHash,userController.Register);
+app.post('/login',authController.validation,authController.passwordChecker, authController.jwtTokenGen);
+app.delete('/users/:id', userController.deleteuser);
+// app.delete('/users/:id', authController.verifyToken, userController.deleteuser);
+app.listen(3002);
